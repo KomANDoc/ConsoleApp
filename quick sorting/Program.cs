2eb989d5fd1cc26b59be248fa4f2835e463b8d8d
Я <@DESKTOP-MI55O4C>
2023-01-18 18:02:54 +0300

    Add project files.

---
+++ quick sorting/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Runtime.CompilerServices;
+using static System.Console;
+
+
+int[] a = { 7, -2, 3, 12, 2, 6, 5};
+
+QuickSort(a);
+
+for(int i = 0; i < a.Length - 1; i++)
+{
+    WriteLine(a[i]);
+    WriteLine();
+}
+
+void QuickSort(int[] a)
+{
+    QuickSortIn(a, 0, a.Length - 1);
+}
+void QuickSortIn(int[] a, int start, int end)
+{
+    if (start >= end)
+    {
+        return;
+    }
+
+    int i = start;
+    int j = end;
+    int num = a[start];
+
+    while (i < j)
+    {
+        while (i < j && a[j] > num)
+        {
+            j--;
+        }
+
+        a[i] = a[j];
+
+        while (i < j && a[i] < num)
+        {
+            i++;
+        }
+
+        a[j] = a[i];
+    }
+
+    a[i] = num;
+    QuickSortIn(a, start, i - 1);
+    QuickSortIn(a, i + 1, end);
+}
---
+++ №1/Program.cs
@@ -0,0 +1,209 @@
+﻿using System.Runtime.CompilerServices;
+using static System.Console;
+
+int[,] doska = new int[8, 8];
+for (int f = 0; f < 8; f++)
+{
+    for (int k = 0; k < 8; k++)
+    {
+        doska[f, k] = 1;
+    }
+}
+for (int f = 0; f < 8; f++)
+{
+    for (int k = 0; k < 8; k++)
+    {
+        Write($"{doska[f, k]} ");
+    }
+    WriteLine();
+}
+WriteLine();
+
+int[] hod = new int[65];
+int schetchik = 0;
+int operation = 0;
+int i, j,x,y;
+i = j = x = y =  0;
+while(schetchik!=64)
+{
+    for (int f = 0; f < 64; f++)
+    {
+        hod[f] = 0;
+    }
+    for (int f = 0; f < 8; f++)
+    {
+        for (int k = 0; k < 8; k++)
+        {
+            doska[f, k] = 1;
+        }
+    }
+    i = y;
+    j = x;
+    schetchik = 0;
+    doska[y,x] = 0;
+    hod_koni();
+    WriteLine($"Понадобилось столько ходов: {schetchik}");
+    x++;
+    if (x > 7)
+    {
+        y++;
+        x = 0;
+    }
+    if (y > 7)
+    {
+        x=y=0;
+        WriteLine("Не правильные ходы :)");
+        break;
+    }
+}
+
+
+
+void hod_koni()
+{
+    while (schetchik < 64)
+    {
+        bool operation_bool = true;
+        int oshibka = 0;
+        operation = hod[schetchik];
+        while (operation_bool)
+        {
+            if (oshibka > 32)
+            {
+                WriteLine("Critical ER hod_koni");
+                break;
+            }
+            switch (operation)
+            {
+                case 0: if (prav_hod(i - 2, j + 1)) { i -= 2; j += 1; hod[schetchik] = operation; schetchik++; operation_bool = false; }
+                    else { operation++; };
+                    break;
+
+                case 1: if (prav_hod(i - 1, j + 2)) { i -= 1; j += 2; hod[schetchik] = operation; schetchik++; operation_bool = false; }
+                    else { operation++; };
+                    break;
+
+                case 2: if (prav_hod(i + 1, j + 2)) { i += 1; j += 2; hod[schetchik] = operation; schetchik++; operation_bool = false; }
+                    else { operation++; };
+                    break;
+
+                case 3: if (prav_hod(i + 2, j + 1)) { i += 2; j += 1; hod[schetchik] = operation; schetchik++; operation_bool = false; }
+                    else { operation++; };
+                    break;
+
+                case 4: if (prav_hod(i + 2, j - 1)) { i += 2; j -= 1; hod[schetchik] = operation; schetchik++; operation_bool = false; }
+                    else { operation++; };
+                    break;
+
+                case 5: if (prav_hod(i + 1, j - 2)) { i += 1; j -= 2; hod[schetchik] = operation; schetchik++; operation_bool = false; }
+                    else { operation++; };
+                    break;
+
+                case 6: if (prav_hod(i - 1, j - 2)) { i -= 1; j -= 2; hod[schetchik] = operation; schetchik++; operation_bool = false; }
+                    else { operation++; };
+                    break;
+
+                case 7: if (prav_hod(i - 2, j - 1)) { i -= 2; j -= 1; hod[schetchik] = operation; schetchik++; operation_bool = false; }
+                    else { operation++; };
+                    break;
+
+                default: obratniy_hod_kone(); oshibka++; break;
+            }
+            
+        }
+        if (oshibka > 3)
+        {
+            break;
+        }
+        doska[i, j] = 0;
+        for (int f = 0; f < 8; f++)
+        {
+            for (int k = 0; k < 8; k++)
+            {
+                Write($"{doska[f, k]} ");
+            }
+            WriteLine();
+        }
+        WriteLine($"\n Ход: {schetchik}\n\n");
+        hod_koni();
+    }
+}
+
+
+void obratniy_hod_kone()
+{
+    hod[schetchik] = 0;
+    schetchik--;
+    switch (hod[schetchik])
+    {
+        case 0: 
+            //if (prav_hod(i + 2, j - 1)) 
+            { i += 2; j -= 1;}
+            //else { WriteLine("Critical ER obrat_hod"); schetchik++; };
+            break;
+        case 1: 
+           // if (prav_hod(i + 1, j - 2)) 
+            { i += 1; j -= 2;}
+            //else { WriteLine("Critical ER obrat_hod"); schetchik++; };
+            break;
+        case 2: 
+           // if (prav_hod(i - 1, j - 2)) 
+            { i -= 1; j -= 2;}
+            //else { WriteLine("Critical ER obrat_hod"); schetchik++; };
+            break;
+        case 3: 
+            //if (prav_hod(i - 2, j - 1)) 
+            { i -= 2; j -= 1;}
+            //else { WriteLine("Critical ER obrat_hod"); schetchik++; };
+            break;
+        case 4: 
+            //if (prav_hod(i - 2, j + 1)) 
+            { i -= 2; j += 1;}
+            //else { WriteLine("Critical ER obrat_hod"); schetchik++; };
+            break;
+        case 5: 
+            //if (prav_hod(i - 1, j + 2)) 
+            { i -= 1; j += 2;}
+            //else { WriteLine("Critical ER obrat_hod"); schetchik++; };
+            break;
+        case 6: 
+            //if (prav_hod(i + 1, j + 2)) 
+            { i += 1; j += 2;}
+            //else { WriteLine("Critical ER obrat_hod"); schetchik++; };
+            break;
+        case 7: 
+            //if (prav_hod(i + 2, j + 1)) 
+            { i += 2; j += 1;}
+            //else { WriteLine("Critical ER obrat_hod"); schetchik++; };
+            break;
+        default: WriteLine("Critical ER obrat_hod_finel"); schetchik++; break;
+    }
+    hod[schetchik]+=1;
+    operation = hod[schetchik];
+    doska[i, j] = 1;
+}
+
+bool prav_hod(int i,int j)
+    {
+        bool otv = true;
+        if (i < 0 || i >= 8)
+        {
+            otv = false;
+            return otv;
+        }
+
+
+        else if (j < 0 || j >= 8)
+        {
+            otv = false;
+            return otv;
+        }
+
+        else if (doska[i, j] == 0)
+        {
+            otv = false;
+            return otv;
+        }
+
+        return otv;
+    }
